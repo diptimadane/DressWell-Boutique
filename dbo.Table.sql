76d6ab919547ef52075e87e2450bcbe068a1aa25
diptimadane <DDMadane@my.harrisburgu.edu>
2016-05-25 21:03:52 -0400

    Data table script

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBL_CustomerInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Address] NVARCHAR(50) NULL, 
+    [Gender] NVARCHAR(50) NULL, 
+    [Name] NVARCHAR(50) NULL, 
+    [PrefTimeOfDelivery] NVARCHAR(50) NULL, 
+    [PrevOrder] NVARCHAR(50) NULL, 
+    [Style] NVARCHAR(50) NULL
+)
